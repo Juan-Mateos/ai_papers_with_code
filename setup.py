21099df84d7efd33150c052b8afa7374b0bed7f8
Juan Mateos-Garcia <juan.mateos-garcia@nesta.org.uk>
2021-10-10 10:58:18 +0100

    Setup Nesta Data Science cookiecutter

---
+++ setup.py
@@ -0,0 +1,26 @@
+"""ai_papers_with_code."""
+from pathlib import Path
+from setuptools import find_packages
+from setuptools import setup
+
+
+def read_lines(path):
+    """Read lines of `path`."""
+    with open(path) as f:
+        return f.read().splitlines()
+
+
+BASE_DIR = Path(__file__).parent
+
+
+setup(
+    name="ai_papers_with_code",
+    long_description=open(BASE_DIR / "README.md").read(),
+    install_requires=read_lines(BASE_DIR / "requirements.txt"),
+    extras_require={"dev": read_lines(BASE_DIR / "requirements_dev.txt")},
+    packages=find_packages(exclude=["docs"]),
+    version="0.1.0",
+    description="Exploration of papers with code data",
+    author="Juan Mateos-Garcia",
+    license="MIT",
+)
